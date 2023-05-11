8ff28a5c293842c98333b34f5959e6d5056fdfd4
EICAcademyAustralia <132050271+EICAcademyAustralia@users.noreply.github.com>
2023-05-11 10:41:51 +1000

    Create example.q1.py

---
+++ Question 1/example.q1.py
@@ -0,0 +1,11 @@
+# This file should only contain the function itself.
+
+from typing import List
+
+# author: EIC Education
+# date: 2023-05-01
+
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        return True
